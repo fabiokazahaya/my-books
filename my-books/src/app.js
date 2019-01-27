14cb11530d79b1a65173303c11693dfd3c2b6c6b
Fábio Kazahaya <fabiokazahaya@Fabios-Air.home>
2019-01-27 20:53:13 +0000

    2.3 - One-way and two-way data binding and event delegation

--- my-books/src/app.js
+++ my-books/src/app.js
@@ -1,5 +1,12 @@
 export class App {
   constructor() {
-    this.message = 'Hello World!';
+    this.books = [];
+    this.bookTitle = "";
+  }
+
+  addBook() {
+    this.books.push({title : this.bookTitle});
+    this.bookTitle = "";
+    console.log("Book list ", this.books);
   }
 }
