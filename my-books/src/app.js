62c662a82c7202fca40ea78258d3425374c1cabc
Fábio Kazahaya <fabiokazahaya@Fabios-Air.home>
2019-02-03 12:44:41 +0000

    Adding router.

--- my-books/src/app.js
+++ my-books/src/app.js
@@ -1,23 +1,11 @@
-import {bindable, inject} from 'aurelia-framework';
-import {BookApi} from 'book-api';
-
-@inject(BookApi)
 export class App {
 
-  constructor(bookApi) {
-    this.bookTitle = "";
-    this.books = [];
-    this.bookApi = bookApi;
-
-  }
-
-  addBook() {
-    this.books.push({title : this.bookTitle});
-    this.bookTitle = "";
-  }
-
-  bind() {
-    this.bookApi.getBooks().then(savedBooks =>
-      this.books = savedBooks);
+  configureRouter(config, router) {
+    this.router = router;
+    config.title = 'my-books';
+    config.map([
+      {route:['','home'], name: 'home', moduleId: 'index'},
+      {route:'books', name: 'books', moduleId: 'books'},
+    ]);
   }
 }
---
+++ my-books/src/books.js
@@ -0,0 +1,21 @@
+import {bindable, inject} from 'aurelia-framework';
+import {BookApi} from 'book-api';
+
+@inject(BookApi)
+export class Books {
+
+  constructor(bookApi) {
+    this.bookTitle = "";
+    this.books = [];
+    this.bookApi = bookApi;
+  }
+
+  addBook() {
+    this.books.push({title : this.bookTitle});
+    this.bookTitle = "";
+  }
+
+  bind() {
+    this.bookApi.getBooks().then(savedBooks=> this.books = savedBooks);
+  }
+}
