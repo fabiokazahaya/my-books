c529d2c81850156f5b441580547063f3bfe9fea2
Fábio Kazahaya <fabiokazahaya@Fabios-Air.home>
2019-02-06 20:53:38 +0000

    Moving files to best location folder.

--- my-books/src/app.js
+++ my-books/src/app.js
@@ -4,8 +4,8 @@ export class App {
     this.router = router;
     config.title = 'my-books';
     config.map([
-      {route:['','home'], name: 'home', moduleId: 'index'},
-      {route:'books', name: 'books', moduleId: 'books'},
+      {route:['','home'], name: 'home', moduleId: './resources/elements/index'},
+      {route:'books', name: 'books', moduleId: './resources/elements/books'},
     ]);
   }
 }
